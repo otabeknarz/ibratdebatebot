2e6aa7f00bbec40734aafe776a64ced6f90d4a0a
Abrorbek <a.nematov2000@gmail.com>
2024-02-07 10:27:43 +0500

    First release

--- main.py
+++ main.py
@@ -1,88 +1,266 @@
-import telebot
-from telebot import types
+import asyncio
+import logging
+import sys
+from os import getenv
+from typing import Any, Dict
+from aiogram import Bot, Dispatcher, F, Router, html
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
 
-bot = telebot.TeleBot("6399662421:AAGYRA_YIm52fNe5dyl1uHVNONbfDf9RhXU")
+TOKEN = getenv("BOT_TOKEN")
+bot = Bot(token="6731464739:AAF0MjIuQPQDSsruL3-bTtWN6z4G8S6AGGA", parse_mode=ParseMode.HTML)
 
-
-def start_markup(status):
-    markup = types.InlineKeyboardMarkup(row_width=True)
-    link_keyboard = types.InlineKeyboardButton(text="Barcha kanalga bittada qo'shilish", url="https://t.me/addlist/dlpwMmHRLdFhMDAy")
-    link_to_the_channel = types.InlineKeyboardButton(text="Marathon kanali", url="https://t.me/+-s5zqa6qkXpjYmFi")
-    check_keyboard = types.InlineKeyboardButton(text="Tekshirish ✅", callback_data="check")
-    link_keyboard1 = types.InlineKeyboardButton(text="Azizbek Zaripov", url="t.me/akzaripovs")
-    link_keyboard2 = types.InlineKeyboardButton(text="Abrorbek Nematov", url="https://t.me/Nematov_Abrorbek")
-    link_keyboard3 = types.InlineKeyboardButton(text="Adilkhan Kenpeilov", url="https://t.me/adilwithin")
-    link_keyboard4 = types.InlineKeyboardButton(text="Azizbek Zaylobiddinov", url="https://t.me/abdulazizziy")
-    if( status == True ):
-        markup.add(link_keyboard, link_keyboard1, link_keyboard2, link_keyboard3, link_keyboard4, check_keyboard)
-    elif( status == False):
-        markup.add(link_to_the_channel)
-
-    return markup
- 
-
-# def give_link():
-#     markup1 = types.InlineKeyboardMarkup(row_width=True)
-#     link_to_the_channel = types.InlineKeyboardButton(text="Way to 1500+", url="https://t.me/+yNK1vLdpprY0YjRi")
-#     markup1.add(link_to_the_channel)
-#     return markup1
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    chat_id = message.chat.id
-    first_name = message.chat.first_name
-    bot.send_message(chat_id, f"Assalomu alaykum {first_name}!\n \n"
-                              f"Marathonga qiziqganingizdan xursandmiz. Bepul SAT Marathonimizga qo'shilish uchun quyidagi kanallarga obuna bo'lishingiz kerak 👇\n \n"
-                              f"Qo'shilgandan so'ng, tekshirish tugmasini bosing. So'ng, bot sizga Marathonga qo'shilish uchun Link beradi.", reply_markup=start_markup(True))
+form_router = Router()
 
 
-def check(call):
-    status = ['creator', 'administrator', 'member']
-    for i in status:
-        if i == bot.get_chat_member(chat_id="-1001861916867", user_id=call.message.chat.id).status:
-            check2(call)
-            break
+class Form(StatesGroup):
+    lang = State()
+    region = State()
+    date = State()
+    name = State()
+    number = State()
+    username = State()
 
+
+@form_router.message(CommandStart())
+async def command_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(Form.lang)
+    await message.answer(
+        f"Tilni tanlang / Select the language",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Uzbek"),
+                    KeyboardButton(text="English"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+
+@form_router.message(Command("cancel"))
+@form_router.message(F.text.casefold() == "cancel")
+async def cancel_handler(message: Message, state: FSMContext) -> None:
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    logging.info("Cancelling state %r", current_state)
+    await state.clear()
+    await message.answer(
+        "Cancelled.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+
+
+
+@form_router.message(Form.lang, F.text.casefold() == "uzbek")
+async def Uzbek(message: Message, state: FSMContext) -> None:
+    await state.update_data(lang="Uzbek")
+    await state.set_state(Form.region)
+
+    await message.answer(
+        f"Qaysi bir hududdagi klubga qatnashmoqchisiz ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Toshkent"),
+                    KeyboardButton(text="Samarqand"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+@form_router.message(Form.lang, F.text.casefold() == "english")
+async def English(message: Message, state: FSMContext) -> None:
+    await state.update_data(lang="English")
+    await state.set_state(Form.region)
+
+    await message.answer(
+        f"Where do you want to participate ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="Tashkent"),
+                    KeyboardButton(text="Samarkand"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+
+@form_router.message(Form.region, F.text.casefold() == "toshkent")
+async def toshkent(message: Message, state: FSMContext) -> None:
+    await state.update_data(region="Toshkent")
+    await state.set_state(Form.date)
+
+    await message.answer(
+        f"Qaysi bir kuni debatega qatnashmoqchisiz ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="09.02.2024 16:00dagi"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+@form_router.message(Form.region, F.text.casefold() == "samarqand")
+async def samarqand(message: Message, state: FSMContext) -> None:
+    await state.update_data(region="Samarqand")
+    await state.set_state(Form.date)
+
+    await message.answer(
+        f"Qaysi bir kuni debatega qatnashmoqchisiz ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="09.02.2024 13:30dagi"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+
+@form_router.message(Form.region, F.text.casefold() == "tashkent")
+async def toshkent(message: Message, state: FSMContext) -> None:
+    await state.update_data(region="Toshkent")
+    await state.set_state(Form.date)
+
+    await message.answer(
+        f"Which session you want to attend ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="on 09.02.2024 16:00"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+@form_router.message(Form.region, F.text.casefold() == "samarkand")
+async def samarqand(message: Message, state: FSMContext) -> None:
+    await state.update_data(region="Samarqand")
+    await state.set_state(Form.date)
+
+    await message.answer(
+        f"Which session you want to attend ?",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(text="on 09.02.2024 13:30"),
+                ]
+            ],
+            resize_keyboard=True,
+        ),
+    )
+
+
+@form_router.message(Form.date)
+async def process_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(date="09.02.2024")
+    await state.set_state(Form.name)
+    data = await state.get_data()
+    language = data.get('lang', 'N/A')
+    if(language == "Uzbek"):
+        await message.answer(text="Ro’yxatdan o’tish uchun F.I.O yozing", reply_markup=ReplyKeyboardRemove(),)
     else:
-        bot.send_message(call.message.chat.id, "Xali siz barcha kanalga obuna bo'lmagansiz. Iltimos barcha kanallarga obuna bo'ling va keyin tekshirish tugmasini bosing", reply_markup=start_markup(True))
+        await message.answer(text="To register write down your full name", reply_markup=ReplyKeyboardRemove(),)
 
-def check2(call):
-    status = ['creator', 'administrator', 'member']
-    for i in status:
-        if i == bot.get_chat_member(chat_id="-1001924103206", user_id=call.message.chat.id).status:
-            check3(call)
-            break
 
+    
+@form_router.message(Form.name)
+async def process_number(message: Message, state: FSMContext) -> None:
+    await state.update_data(name=message.text)
+    await state.set_state(Form.number)
+    data = await state.get_data()
+    language = data.get('lang', 'N/A')
+    if(language == "Uzbek"):
+        await message.answer(text="Endi telefon raqamingizni kiriting", reply_markup=ReplyKeyboardRemove(),)
     else:
-        bot.send_message(call.message.chat.id, "Xali siz barcha kanalga obuna bo'lmagansiz. Iltimos barcha kanallarga obuna bo'ling va keyin tekshirish tugmasini bosing", reply_markup=start_markup(True))
-
-def check3(call):
-    status = ['creator', 'administrator', 'member']
-    for i in status:
-        if i == bot.get_chat_member(chat_id="-1001618038051", user_id=call.message.chat.id).status:
-            check4(call)
-            break
-
-    else:
-        bot.send_message(call.message.chat.id, "Xali siz barcha kanalga obuna bo'lmagansiz. Iltimos barcha kanallarga obuna bo'ling va keyin tekshirish tugmasini bosing", reply_markup=start_markup(True))
-        
-def check4(call):
-    status = ['creator', 'administrator', 'member']
-    for i in status:
-        if i == bot.get_chat_member(chat_id="-1001825051597", user_id=call.message.chat.id).status:
-            bot.send_message(call.message.chat.id, "Juda zo'r 👏. Bepul SAT Marathonga qo'shilganingiz bilan tabriklaymiz. Pastki tugmadagi link orqali marathon kanaliga qo'shiling 👇", reply_markup=start_markup(False))
-        
-            break
-
-    else:
-        bot.send_message(call.message.chat.id, "Xali siz barcha kanalga obuna bo'lmagansiz. Iltimos barcha kanallarga obuna bo'ling va keyin tekshirish tugmasini bosing", reply_markup=start_markup(True))
+        await message.answer(text="Please, write your phone number", reply_markup=ReplyKeyboardRemove(),)
 
 
-@bot.callback_query_handler(func= lambda call: True)
-def callback(call):
-    if call.data == 'check':
-        bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
-        check(call)
+
+@form_router.message(Form.number)
+async def send_user_info(message: Message, state: FSMContext) -> None:
+    await state.update_data(number=message.text)
+
+    # getting the user name
+    await state.update_data(username = message.from_user.username)
 
 
-bot.polling(none_stop=True)
+    data = await state.get_data()
+    message_text = f"User Information:\n\n"
+    message_text += f"Region: {data.get('region', 'N/A')}\n"
+    message_text += f"Date: {data.get('date', 'N/A')}\n"
+    message_text += f"Name: {data.get('name', 'N/A')}\n"
+    message_text += f"telegram username: @{data.get('username', 'N/A')}\n"
+    message_text += f"Phone Number: {data.get('number', 'N/A')}\n"
+
+    channel_id_tashkent = -1002079456954  # data channel id
+    channel_id_samarkand = -1002103687550
+    group_link = ""
+    if data.get('region', 'N/A') == "Toshkent":
+        await bot.send_message(channel_id_tashkent, message_text)
+        group_link= "https://t.me/+9FsbILdXfWkwZGYy"
+    elif data.get('region', 'N/A') == "Samarqand":
+        await bot.send_message(channel_id_samarkand, message_text)
+        group_link= "https://t.me/+C5s6G4wWnMw2OTIy"
+    else: return True
+
+
+    user_id = message.from_user.id
+    language = data.get('lang', 'N/A')
+    if language == "Uzbek" :
+        message_text = (
+            f"Tabriklaymiz! Siz muvaffaqiyatli ro’yxatdan o’tdingiz.\n\n"
+            f"Iltimos, klub vaqti, joylashuvi va shu kabi ko’proq ma’lumotlarni olish uchun guruxga qo’shiling:\n"
+            f"{group_link}"
+        )
+    else: 
+        message_text = (
+            f"Congratulations! You have successfully registered.\n\n"
+            f"Please join the group for club times, locations and more information like:\n"
+            f"{group_link}\n"
+            f"(Joining the group is required)"
+        )
+
+    await bot.send_message(user_id, message_text, parse_mode=ParseMode.MARKDOWN)
+
+
+
+
+
+
+async def main():
+    # bot = Bot(token="6731464739:AAF0MjIuQPQDSsruL3-bTtWN6z4G8S6AGGA", parse_mode=ParseMode.HTML)
+    dp = Dispatcher()
+    dp.include_router(form_router)
+    await dp.start_polling(bot)
+
+    
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
