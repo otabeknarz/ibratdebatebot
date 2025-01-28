a652988eb5decae4a1f9be88af48677eb7fafc90
Elbek Rahimjon <bekelbek2@gmail.com>
2025-01-28 18:42:20 +0500

    added patch method request function

--- modules/functions.py
+++ modules/functions.py
@@ -1,9 +1,12 @@
 import requests
 
-
-def get_req(url):
+def get_req(url: str) -> requests.Response:
     return requests.get(url)
 
 
-def post_req(url, json):
+def post_req(url: str, json: dict) -> requests.Response:
     return requests.post(url=url, json=json)
+
+
+def patch_req(url: str, json: dict) -> requests.Response:
+    return requests.patch(url=url, json=json)
