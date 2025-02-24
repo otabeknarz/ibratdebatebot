c6830e44fb3fade551574b239a56ba0c0b2006b7
Otabek Narz <otabeknarz@gmail.com>
2025-02-24 17:13:58 +0500

    added create_user() and update_user() functions

--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,36 @@
 import requests
+import aiohttp
+
+from . import settings
+
+bot_settings = settings.Settings()
+
+
+async def create_user(id: str, name: str, username: str) -> aiohttp.ClientResponse:
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(bot_settings.CREATE_USER_URL, json={"id": id, "name": name, "username": username})
+    return response
+
+
+async def update_user(
+    id: str,
+    name: str,
+    username: str,
+    phone_number: str,
+    english_level: str
+) -> aiohttp.ClientResponse:
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            bot_settings.UPDATE_USER_URL,
+            json={
+                "id": id,
+                "name": name,
+                "username": username,
+                "phone_number": phone_number,
+                "english_level": english_level
+            }
+        )
+    return response
 
 def get_req(url: str) -> requests.Response:
     return requests.get(url)
